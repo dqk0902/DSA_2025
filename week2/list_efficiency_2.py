d1f834ffeca00ca532a0017262b2a156c0910b96
dqk0902 <duongquockhang.1205@gmail.com>
2024-12-08 17:14:10 +0200

    some tests

---
+++ week2/list_efficiency_2.py
@@ -0,0 +1,21 @@
+import time
+
+def test_list_efficiency(n):
+    list = []
+
+    start_add_time = time.time()
+    for i in range(1, n+1):
+        list.append(i)
+    add_time = time.time() - start_add_time
+
+    start_delete_time =  time.time()
+    for i in range(n):
+        list.pop(0)
+    delete_time = time.time() - start_delete_time
+
+    print(f"Time taken to append {n} elements: {add_time:.3f} seconds")
+    print(f"Time taken to delete {n} elements: {delete_time:.3f} seconds")
+
+if __name__ == "__main__":
+    n = 10**5
+    test_list_efficiency(n)
