86c246273415b7d361dcb420b68c186b0e3d4cd4
dqk0902 <duongquockhang.1205@gmail.com>
2024-12-16 22:28:13 +0200

    my algorithm, not copilot

--- week2/circlegame.py
+++ week2/circlegame.py
@@ -12,24 +12,35 @@ def create(n):
             even_index.append(i)
         else:
             odd_index.append(i)
-
-    ## first round in the circle
-    if n % 2 == 0 or n == 1:
-        for index in even_index:
-            removed_list.append(circle[index])
-        for index in even_index:
-            circle.pop(index)
-    else:
-        for index in odd_index:
-            removed_list.append(circle[index])
-        for index in odd_index:
-            circle.pop(index)
-
     
-    print('circle', circle)
+    if n == 1:
+      removed_list.append(circle[0])
+    else: 
+        for index in odd_index:
+            removed_list.append(circle[index])
+        for index in range(1, len(odd_index)+1):
+            circle.pop(index)
+        while len(circle) > 0:
+            if len(circle) % 2 == 0 or len(circle) == 1:
+                for index in even_index:
+                    if index < len(even_index) and index < len(circle):
+                        removed_list.append(circle[index])
+                for index in range(len(even_index)):
+                    if index < len(even_index) and index < len(circle):
+                        circle.pop(index)
+            else:
+                for index in odd_index:
+                    if index < len(odd_index) and index < len(circle):
+                        removed_list.append(circle[index])
+                for index in range(1, len(odd_index)):
+                    if index < len(odd_index) and index < len(circle):
+                        circle.pop(index)        
+
     return removed_list
 
 if __name__ == "__main__":
     print(create(1)) # [1]
     print(create(3)) # [2,1,3]
+    print(create(4)) # [2,4,1,3]
+    print(create(6)) # [2,4,6,3,1,5]
     print(create(7)) # [2,4,6,1,5,3,7]
