348c9c0345060c2d38942d4ade65ad34b0bf50eb
dqk0902 <duongquockhang.1205@gmail.com>
2024-12-17 12:30:54 +0200

    add test and fix algo

--- week2/circlegame.py
+++ week2/circlegame.py
@@ -13,28 +13,17 @@ def create(n):
         else:
             odd_index.append(i)
     
-    if n == 1:
-      removed_list.append(circle[0])
-    else: 
-        for index in odd_index:
-            removed_list.append(circle[index])
-        for index in range(1, len(odd_index)+1):
-            circle.pop(index)
-        while len(circle) > 0:
-            if len(circle) % 2 == 0 or len(circle) == 1:
-                for index in even_index:
-                    if index < len(even_index) and index < len(circle):
-                        removed_list.append(circle[index])
-                for index in range(len(even_index)):
-                    if index < len(even_index) and index < len(circle):
-                        circle.pop(index)
-            else:
-                for index in odd_index:
-                    if index < len(odd_index) and index < len(circle):
-                        removed_list.append(circle[index])
-                for index in range(1, len(odd_index)):
-                    if index < len(odd_index) and index < len(circle):
-                        circle.pop(index)        
+    for index in odd_index:
+        removed_list.append(circle[index])
+    for index in range(1, len(odd_index)+1):
+        circle.pop(index)
+    while len(circle) > 0:
+        for index in even_index:
+            if index < len(even_index) and index < len(circle):
+                removed_list.append(circle[index])
+        for index in range(len(even_index)):
+            if index < len(even_index) and index < len(circle):
+                circle.pop(index)    
 
     return removed_list
 
@@ -42,5 +31,9 @@ if __name__ == "__main__":
     print(create(1)) # [1]
     print(create(3)) # [2,1,3]
     print(create(4)) # [2,4,1,3]
-    print(create(6)) # [2,4,6,3,1,5]
+    print(create(5)) # [2,4,1,5,3]
+    print(create(6)) # [2,4,6,1,5,3]
     print(create(7)) # [2,4,6,1,5,3,7]
+    print(create(8)) # [2,4,6,8,1,5,3,7]
+    print(create(9)) # [2,4,6,8,1,5,9,3,7]
+    print(create(10)) # [2,4,6,8,10,1,5,9,3,7]
